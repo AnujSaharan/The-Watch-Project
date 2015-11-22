e93ef9774c8059f4b0d636bbc916d3d320d3de22
Anuj Saharan <anujsinghsaharan@gmail.com>
2015-11-22 12:37:04 +0530

    Update Main.cpp
    v0.2

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,38 @@
+//The Watch Project v0.2
+
 #include <iostream>
 #include <ctime>
 #include <unistd.h>
 #include <windows.h>
+#include "config.cpp"
 using namespace std;
 
 int Hour,Minute,Second;
-int Day,Month,Year;
+int Date,Month,Year,Day_Of_Week;
+char AM_or_PM;
 
 void Time_Input() //Date and Time Input using CTime
 {
-time_t currentTime;
-struct tm *localTime;
+time_t Current_Time;
+struct tm *Local_Time;
 
-time(&currentTime);
-localTime=localtime(&currentTime);
+time(&Current_Time);
+Local_Time=localtime(&Current_Time); //Gets the Local Time
 
-Hour=(localTime->tm_hour)%12; //%12 is for 12 Hour Format
-Minute=(localTime->tm_min);
-Second=(localTime->tm_sec);
+Hour=(Local_Time->tm_hour); //Hour is 24 hour format to get AM or PM
+if(Hour<=12) {AM_or_PM='A';} else {AM_or_PM='P';} //A=AM and P=PM
 
-Day=(localTime->tm_mday);
-Month=(localTime->tm_mon+1);
-Year=(localTime->tm_year+1900);
+Hour=(Local_Time->tm_hour)%12; //Gets the Hour of the Day, %12 is for 12 Hour Format
+Minute=(Local_Time->tm_min); //Gets the Minute of the Hour
+Second=(Local_Time->tm_sec); //Gets the Second of the Minute
+
+Day_Of_Week=(Local_Time->tm_wday);  //Gets the Day of the Week
+Date=(Local_Time->tm_mday); //Gets the Day of the Month
+Month=(Local_Time->tm_mon+1); //Gets the Month of the Year
+Year=(Local_Time->tm_year+1900); //Gets the Year
 }
 
-void Time_Output() //Time Output in Words
+void Time_Output() //Date and Time Output in Words
 {
 string Hour_Text[]={"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"};
 
@@ -41,30 +49,43 @@ string Second_Text[]={"zero seconds","one second","two seconds","three seconds",
                       "forty four seconds","forty five seconds","forty six seconds","forty seven seconds","forty eight seconds","forty nine seconds","fifty seconds","fifty one seconds",
                       "fifty two seconds","fifty three seconds","fifty four seconds","fifty five seconds","fifty six seconds","fifty seven seconds","fifty eight seconds","fifty nine seconds"};
 
+string Day_Text[]={"sunday","monday","tuesday","wednesday","thursday","saturday"};
+
+string Month_Text[]={"january","february","march","april","may","june","july","august","september","october","november","december"};
+
+string Date_Text[]={"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty",
+                    "twenty one","twenty two","twenty three","twenty four","twenty five","twenty six","twenty seven","twenty eight","twenty nine","thirty","thirty one"};
+
+string Year_Prefix_Text[]={"one thousand and","two thousand and","three thousand and","four thousand and","five thousand and","six thousand and","seven thousand and","eight thousand and","nine thousand and"};
+string Year_Suffix_Text[]={"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen",
+                           "twenty","twenty one","twenty two","twenty three","twenty four","twenty five","twenty six","twenty seven","twenty eight","twenty nine","thirty"};
+
 for(;;)
     {
     Time_Input();
 
     if(Minute==60) {Hour++;}
     if(Second==60) {Minute++;}
+    if((Hour==12)&&(AM_or_PM='A')) {Date++;}
 
-    if(Minute==0) {cout<<Hour_Text[Hour-1]<<" o' clock and\n"<<Second_Text[Second-1];}
+    cout<<Day_Text[Day_Of_Week]<<"\n\n"; //Displays the Day of the Week
 
-    if(((Minute>0)&&(Minute<=30))&&(Second==0)) {cout<<Minute_Text[Minute-1]<<"\npast\n"<<Hour_Text[Hour-1];}
-    if(((Minute>0)&&(Minute<=30))&&(Second>0)) {cout<<Minute_Text[Minute-1]<<"\nand "<<Second_Text[Second]<<"\npast "<<Hour_Text[Hour-1];}
+    if(Minute==0) {cout<<Hour_Text[Hour-1]<<" o' clock and\n"<<Second_Text[Second-1]<<"\n\n";} //Displays the time, "twelve o' clock and thirty seconds"
 
-    if(((Minute>30)&&(Minute<=60))&&(Second==0)) {cout<<Minute_Text[59-Minute]<<"\nto\n"<<Hour_Text[Hour];}
-    if(((Minute>30)&&(Minute<=60))&&(Second>0)) {cout<<Minute_Text[59-Minute]<<"\nand "<<Second_Text[60-Second]<<"\nto "<<Hour_Text[Hour];}
+    if(((Minute>0)&&(Minute<=30))&&(Second==0)) {cout<<Minute_Text[Minute-1]<<"\npast\n"<<Hour_Text[Hour-1]<<"\n\n";} //Displays the time, "twenty minutes past twelve"
+    if(((Minute>0)&&(Minute<=30))&&(Second>0)) {cout<<Minute_Text[Minute-1]<<"\nand "<<Second_Text[Second]<<"\npast "<<Hour_Text[Hour-1]<<"\n\n";} //Displays the time, "twenty minutes and one second past two
 
-    sleep(1); //Freezes the console for 1 second
+    if(((Minute>30)&&(Minute<=60))&&(Second==0)) {cout<<Minute_Text[59-Minute]<<"\nto\n"<<Hour_Text[Hour]<<"\n\n";} //Displays the time, "forty minutes to twelve"
+    if(((Minute>30)&&(Minute<=60))&&(Second>0)) {cout<<Minute_Text[59-Minute]<<"\nand "<<Second_Text[60-Second]<<"\nto "<<Hour_Text[Hour]<<"\n\n";} //Displays the time, "forty minutes and one second to six"
+
+    if(AM_or_PM=='A') {cout<<"ante meridiem";} //Displays AM or PM depending upon the time of the day
+    else {cout<<"post meridiem";} //Displays AM or PM depending upon the time of the day
+
+    cout<<"\n\n"<<Date[Date_Text-1]<<endl<<Month_Text[Month]<<endl<<Year_Prefix_Text[(Year/1000)-1]<<" "<<Year_Suffix_Text[(Year%100)-1]; //Displays the Date, "twenty two november 2015"
+
+    sleep(1); //Freezes the console for a second
     system("cls"); //Clears the screen to update the time
     }
-}
-
-void Date_Output() //Date Output in Numericals
-{
-Time_Input();
-cout<<Day<<" "<<Month<<" "<<Year<<endl;
 }
 
 int main() //Main Function
